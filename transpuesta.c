680031217d1061c7beeb7c195901fc11355aeb62
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-27 16:25:19 -0500

    Matriz original

--- transpuesta.c
+++ transpuesta.c
@@ -22,6 +22,15 @@ int main(int argc, char const *argv[])
             matrise[i][j] = matrio[i][j];
         }
     }
+    printf("La matriz Principal es : \n \n");
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d ", matrio[i][j]);
+        }
+        printf("\n");
+    }
 
-     return 0;
+    return 0;
 }

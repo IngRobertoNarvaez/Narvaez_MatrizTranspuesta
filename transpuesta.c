2b9ba553ddcacd23a231f49d0b01f722a13f72f5
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-27 16:24:42 -0500

    Inicia la matriz aleatoria

--- transpuesta.c
+++ transpuesta.c
@@ -3,14 +3,25 @@
 #include <time.h>
 int main(int argc, char const *argv[])
 {
-    srand(time(NULL));
+
     int filas;
     int columnas;
     printf("Ingrese las filas y columnas : ");
     scanf("%d", &filas);
     printf("Ingrese las Columnas : ");
     scanf("%d", &columnas);
-    int matri[filas][columnas];
+    int matrio[filas][columnas];
+    int matrise[filas][columnas];
+    srand(time(NULL));
 
-    return 0;
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matrio[i][j] = rand() % 101;
+            matrise[i][j] = matrio[i][j];
+        }
+    }
+
+     return 0;
 }
